00b5b0676550fa8e8c9ed0a8210539422b7c9f5c
davvidfrancaa <davidsantana98@outlook.com>
2026-06-08 00:59:59 -0300

    atualizacao de teste do banco de dados

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>AtendeLab</title>
+  <title>David</title>
+</head>
+<body>
+ <h1>AtendeLab</h1>
+ <p>Sistema de Controle de Atendimentos Acadêmicos.</p>
+</body>
+</html>
